7e3045eca0a499336ba953eb36992d6139176c60
Mikhail-Polivakha <mikhailpolivakha@gmail.com>
2021-06-03 11:11:47 +0300

    change SecurityContextHolderStrategy to GLOBAL_MODE

--- src/main/java/com/example/security_context_strategies/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/security_context_strategies/security/ApplicationSecurityConfiguration.java
@@ -1,13 +1,21 @@
 package com.example.security_context_strategies.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReadyEvent() {
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_GLOBAL);
+    }
 
     @Autowired
     private DummyAuthenticationFilter dummyAuthenticationFilter;
